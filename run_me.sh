2f24749c1dfac56b609e08273b4d6be421381143
Jonny1987 <jrshannie@yahoo.com>
2024-07-29 21:03:39 +0100

    Improved run_me, asking for platform to determine whether to copy bashrc or zshrc

--- run_me.sh
+++ run_me.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-[ ! -f ~/.bashr ] || mv ~/.bashrc ~/.bashrc_orig
-ln -fs $(pwd)/.bashrc ~/.bashrc
 
-[ ! -f ~/.vimrc ] || mv ~/.vimrc ~/.vimrc_orig
-ln -fs $(pwd)/.vimrc ~/.vimrc
+# Function to create symbolic link and backup existing file if it exists
+create_link() {
+  local src=$1
+  local dest=$2
+  if [ -f $dest ]; then
+    mv $dest ${dest}_orig
+  fi
+  ln -fs $(pwd)/$src $dest
+}
 
-[ ! -f ~/.gitconfig ] || mv ~/.gitconfig ~/.gitconfig_orig
-ln -fs $(pwd)/.gitconfig ~/.gitconfig
+# Ask user for platform
+read -p "Are you using Linux or macOS? (Enter 'linux' or 'mac'): " platform
+
+# Set the shell configuration file based on the platform
+if [ "$platform" == "linux" ]; then
+  shell_rc=".bashrc"
+elif [ "$platform" == "mac" ]; then
+  shell_rc=".zshrc"
+else
+  echo "Invalid platform specified. Please run the script again and enter 'linux' or 'mac'."
+  exit 1
+fi
+
+# Create links for the shell configuration file, .vimrc, and .gitconfig
+create_link $shell_rc ~/$shell_rc
+create_link .vimrc ~/.vimrc
+create_link .gitconfig ~/.gitconfig
+
+echo "Configuration files have been linked successfully."
